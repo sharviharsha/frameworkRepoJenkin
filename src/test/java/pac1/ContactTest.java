7af8dc01e873067144b5724199b2f87ade71aae8
Lenovo <Lenovo@DESKTOP-764EUV6.mshome.net>
2025-03-03 14:57:01 +0530

    added

---
+++ src/test/java/pac1/ContactTest.java
@@ -0,0 +1,27 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest
+{
+@Test
+public void CreateContactTest()
+{
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser","chrome");
+	String USERNAME=System.getProperty("username","admin");
+	String PASSWORD=System.getProperty("password","admin");
+	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	System.out.println("execute CreateContactTest");
+}
+
+@Test
+public void ModifyContactTest()
+{
+	System.out.println("execute ModifyContactTest");
+}
+}
