bab920ae6eb68f05693ecb72a554306af9668778
Lenovo <Lenovo@DESKTOP-764EUV6.mshome.net>
2025-03-03 17:24:57 +0530

    modify in create org test

--- src/test/java/pac2/OrgTest.java
+++ src/test/java/pac2/OrgTest.java
@@ -7,6 +7,15 @@ public class OrgTest
 	@Test
 	public void CreateOrgTest()
 	{
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser","chrome");
+		String USERNAME=System.getProperty("username","admin");
+		String PASSWORD=System.getProperty("password","admin");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("execute createOrgTest");
 	}
     
